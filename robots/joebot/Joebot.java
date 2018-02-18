2899919f326b3b9b4520009e78ba088c358fa57f
YJoe <YJoe762@gmail.com>
2018-02-18 17:33:26 +0000

    Battles of randomly generated JoeBots running, scores logged to a file for later use

--- robots/joebot/Joebot.java
+++ robots/joebot/Joebot.java
@@ -2,39 +2,140 @@ package joebot;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 import robocode.*;
 
 public class Joebot extends AdvancedRobot {
-	
+
+	ArrayList<Integer> sequenceFunctions = new ArrayList<Integer>();
+	ArrayList<Double> sequenceParams = new ArrayList<Double>();
+	int[] eventFunctions = new int[4];
+	double[] eventParams = new double[4];
+	String filePointerPointsTo;
+
+	private void readDataFile(){
+
+        BufferedReader reader = null;
+        try {
+
+            // Find out what file this robot should load by looking at filePointer.txt
+            reader = new BufferedReader(new FileReader(getDataFile("filePointer.txt")));
+            filePointerPointsTo = reader.readLine();
+
+            // If a reader was here in the first place, get rid of it
+            if(reader != null){
+                reader.close();
+            }
+
+            // Open the file that the filePointer file told us to
+            reader = new BufferedReader(new FileReader(getDataFile(filePointerPointsTo)));
+
+            // Read lines until we see a blank line
+            String line;
+            while (!(line = reader.readLine()).equals("")){
+
+                // Split the line we read into an array of <functioncall><value>
+                String[] lineSplit = line.split(",");
+                for(int i = 0; i < lineSplit.length; i += 2){
+
+                    // Store the line data for later use
+                    sequenceFunctions.add(Integer.parseInt(lineSplit[i]));
+                    sequenceParams.add(Double.parseDouble(lineSplit[i + 1]));
+                }
+            }
+
+            // There must be 4 more commands to read
+            for(int i = 0; i < 4; i++){
+                line = reader.readLine();
+                String[] lineSplit = line.split(",");
+                eventFunctions[i] = Integer.parseInt(lineSplit[0]);
+                eventParams[i] = Double.parseDouble(lineSplit[1]);
+            }
+
+            // If a reader was here in the first place, get rid of it
+            if(reader != null){
+                reader.close();
+            }
+        }
+        catch (IOException e){}
+        catch (NumberFormatException e){}
+	}
+
 	public void run() {
-		int number;
-		BufferedReader reader = null;
-		try {
-			reader = new BufferedReader(new FileReader(getDataFile("data.txt")));
-			number = Integer.parseInt(reader.readLine());
-			if(reader != null){
-				reader.close();
-			}		
-		} catch (IOException e) {
-			number = 0;
-		} catch (NumberFormatException e) {
-			number = 0;
-		}
+
+	    // Get the instructions that this robot should use
+	    readDataFile();
 		
+		// While the robot is alive
 		while(true){
-			back(number);
+		
+			// Cycle through the set of tasks we need to do
+			for(int i = 0; i < sequenceFunctions.size(); i++){
+				
+				// Call a given command
+				evaluate(sequenceFunctions.get(i), sequenceParams.get(i));
+			}
+
+	    	// if any actions were set they need to execute before this function ends the robot may get stuck if not,
+            // when this execute was not in place several robots failed to write their data to the file
+			execute();
 		}
 	}
 	
+	public void evaluate(int functionNumber, double functionValue){
+		switch(functionNumber){
+			case 0:	ahead(functionValue); break;
+			case 1:	back(functionValue); break;
+			case 2:	fire(functionValue); break;
+			case 3:	turnGunLeft(functionValue); break;
+			case 4:	turnGunRight(functionValue); break;
+			case 5:	turnRadarLeft(functionValue); break;
+			case 6:	turnRadarRight(functionValue); break;
+			case 7:	turnLeft(functionValue); break;
+			case 8:	turnRight(functionValue); break;
+			case 9: setAhead(functionValue); break;
+			case 10: setBack(functionValue); break;
+			case 11: setFire(functionValue); break;
+			case 12: setTurnGunLeft(functionValue); break;
+			case 13: setTurnGunRight(functionValue); break;
+			case 14: setTurnLeft(functionValue); break;
+			case 15: setTurnRight(functionValue); break;
+			case 16: setTurnRadarLeft(functionValue); break;
+			case 17: setTurnRadarRight(functionValue); break;
+			case 18: waitFor(new TurnCompleteCondition(this)); break;
+			case 19: waitFor(new GunTurnCompleteCondition(this)); break;
+			case 20: waitFor(new MoveCompleteCondition(this)); break;
+			case 21: waitFor(new RadarTurnCompleteCondition(this)); break;
+            case 22: execute(); break;
+			case 23: doNothing(); break;
+		}
+	}
+
 	public void onScannedRobot(ScannedRobotEvent e) {
-		//fire(1);
+		evaluate(eventFunctions[0], eventParams[0]);
 	}
 
 	public void onHitByBullet(HitByBulletEvent e) {
-		//back(10);
+		evaluate(eventFunctions[1], eventParams[1]);
 	}
 	
 	public void onHitWall(HitWallEvent e) {
-		//back(20);
-	}	
+		evaluate(eventFunctions[2], eventParams[2]);
+	}
+	
+	public void onHitRobot(HitRobotEvent e){
+		evaluate(eventFunctions[3], eventParams[3]);
+	}
+
+    public void onBattleEnded(BattleEndedEvent ev){
+		try{
+	    	RobocodeFileWriter fileWriter = new RobocodeFileWriter(getDataFile("scores.txt").getAbsolutePath(), true);
+	        BattleResults battleResults = ev.getResults();
+			fileWriter.write(filePointerPointsTo + " " + battleResults.getScore() + "\n");
+			fileWriter.close();
+		}
+		catch(IOException e){
+			e.printStackTrace();
+		}
+	}
 }
--- src/main/Main.java
+++ src/main/Main.java
@@ -7,35 +7,190 @@ import robocode.control.RobocodeEngine;
 import robocode.control.RobotSpecification;
 import robocode.control.events.*;
 
-public class Main {
-    public static void main(String[] args){
-        RobocodeEngine.setLogMessagesEnabled(true);
-        RobocodeEngine.setLogErrorsEnabled(true);
+import java.io.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Random;
 
-        RobocodeEngine robocodeEngine = new RobocodeEngine();
-        StringBuilder str = new StringBuilder();
-        for(int i = 0; i < 5; i++){
-            str.append("joebot.Joebot*,");
+public class Main {
+
+    public static String[] createPopulationFiles(int populationSize){
+        int sequenceCountMax = 20;
+        int functionOptions = 22;
+        double functionValMin = 0.0;
+        double functionValMax = 1000.0;
+        Random rand = new Random();
+        String[] fileNames = new String[populationSize];
+
+        for(int i = 0; i < populationSize; i++){
+
+            fileNames[i] = (i + 1) + ".txt";
+
+            PrintWriter writer = null;
+            try {
+                writer = new PrintWriter("../EvoSim/robots/joebot/Joebot.data/" + (i + 1) + ".txt", "UTF-8");
+            } catch (FileNotFoundException | UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
+            assert writer != null;
+
+            int thisSequenceCount = rand.nextInt(sequenceCountMax - 1) + 1;
+            for(int j = 0; j < thisSequenceCount; j++){
+                double functionVal = functionValMin + (functionValMax - functionValMin) * rand.nextDouble();
+                int functionIndex = rand.nextInt(functionOptions);
+                writer.println(functionIndex + "," + functionVal);
+            }
+
+            writer.println();
+
+            for(int j = 0; j < 4; j++){
+                double functionVal = functionValMin + (functionValMax - functionValMin) * rand.nextDouble();
+                int functionIndex = rand.nextInt(functionOptions);
+                writer.println(functionIndex + "," + functionVal);
+            }
+
+            writer.close();
         }
 
-        RobotSpecification[] allRobots = robocodeEngine.getLocalRepository(str.toString());
-        BattleSpecification battleSpecification = new BattleSpecification(new BattlefieldSpecification(), 3,
-                1000, 5, 50, false, allRobots);
+        return fileNames;
+    }
 
-        //robocodeEngine.setVisible(true);
-        robocodeEngine.addBattleListener(new BattleEventDispatcher() {
-            @Override
-            public void onBattleCompleted(BattleCompletedEvent battleCompletedEvent) {
-                BattleResults[] battleResults = battleCompletedEvent.getSortedResults();
-                for(BattleResults b : battleResults) {
-                    System.out.println("SCORE [" + b.getScore() + "] NAME [" + b.getTeamLeaderName() + "]");
-                }
+    public static void setJobotFilePointer(String fileLocation){
+        PrintWriter writer = null;
+        try {
+            writer = new PrintWriter("../EvoSim/robots/joebot/Joebot.data/filePointer.txt", "UTF-8");
+        } catch (FileNotFoundException | UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        assert writer != null;
+
+        writer.println(fileLocation);
+        writer.close();
+
+        System.out.println("JoeBot file is now [" + fileLocation + "]");
+    }
+
+    public static void createJoeBotLog(){
+        PrintWriter writer = null;
+        try {
+            writer = new PrintWriter("../EvoSim/robots/joebot/Joebot.data/scores.txt", "UTF-8");
+        } catch (FileNotFoundException | UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        assert writer != null;
+
+        writer.println("JoeBot Scores");
+        writer.close();
+    }
+
+    public static String getRobotLoadString(int totalBots, int joeBots){
+
+        Random rand = new Random();
+
+        ArrayList<String> botNames = new ArrayList<String>(){{
+            add("sample.Corners");
+            add("sample.Crazy");
+            add("sample.Fire");
+            add("sample.RamFire");
+            add("sample.SittingDuck");
+            add("sample.SpinBot");
+            add("sample.Tracker");
+            add("sample.VelociRobot");
+            add("sample.Walls");
+        }};
+
+        int totalCreated = 0;
+
+        StringBuilder str = new StringBuilder();
+        for(int i = 0; i < joeBots; i++){
+
+            if(totalCreated < totalBots) {
+                str.append("joebot.Joebot*,");
             }
-        });
-        robocodeEngine.runBattle(battleSpecification);
-        robocodeEngine.waitTillBattleOver();
-        robocodeEngine.close();
+        }
 
-        throw new Error("EVERYTHING IS FINE");
+        while (totalCreated < totalBots){
+            int randIndex = rand.nextInt(botNames.size());
+            str.append(botNames.get(randIndex)).append(",");
+            totalCreated++;
+        }
+
+        return str.toString();
+    }
+
+    private static HashMap<String, Integer> getGenerationScores(){
+        HashMap<String, Integer> scores = new HashMap<>();
+
+        try {
+            // Open the scores file
+            BufferedReader reader = new BufferedReader(new FileReader("../EvoSim/robots/joebot/Joebot.data/scores.txt"));
+
+            // Read lines until we see a blank line
+            String line;
+
+            // the first line isn't a result so just ignore it
+            reader.readLine();
+
+            // while we haven't reached the end of the file
+            while ((line = reader.readLine()) != null) {
+                String[] lineSplit = line.split(" ");
+                scores.put(lineSplit[0], Integer.parseInt(lineSplit[1]));
+            }
+
+            // we are done with the file so close it
+            reader.close();
+        }
+        catch (IOException | NumberFormatException e){
+            e.printStackTrace();
+        }
+
+        return scores;
+    }
+
+    public static void main(String[] args){
+
+        // Create an instance of RobocodeEngine to run the battles
+        RobocodeEngine.setLogMessagesEnabled(true);
+        RobocodeEngine.setLogErrorsEnabled(true);
+        RobocodeEngine robocodeEngine = new RobocodeEngine();
+
+        // Create a population of JoeBots
+        int population = 5;
+        String[] robotFiles = createPopulationFiles(population);
+
+        // Get a string of random robots to challenge this round, make sure there is one JoeBot
+        String robotoString = getRobotLoadString(10, 1);
+
+        // Using the string we made earlier, get a collection of robot specifications
+        RobotSpecification[] allRobots = robocodeEngine.getLocalRepository(robotoString);
+
+        // Create a Battle specification using these robots
+        BattleSpecification battleSpecification = new BattleSpecification(new BattlefieldSpecification(), 5,
+                10, 5, 50, false, allRobots);
+
+        // Set if we want to see the UI or not
+        //robocodeEngine.setVisible(true);
+
+        // create (or overwrite) the score logging file for this generation
+        createJoeBotLog();
+
+        // For the entire JoeBot population size
+        for(int i = 0; i < population; i++){
+
+            // Point the given JoeBot to its data file
+            setJobotFilePointer(robotFiles[i]);
+
+            // Run the battle and wait for it to finish before we continue
+            robocodeEngine.runBattle(battleSpecification);
+            robocodeEngine.waitTillBattleOver();
+        }
+
+        // The generation finished their fighting so read the results
+        HashMap<String, Integer> scores = getGenerationScores();
+        for(String key : scores.keySet()){
+            System.out.println("[" + key + "] scored [" + scores.get(key) + "]");
+        }
+
+        robocodeEngine.close();
     }
 }
